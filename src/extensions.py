bdcf5f7a9653187fa173261a5820deae4dff2cb5
M. Istighfar Amal <tifff@MacBook-Air-M.local>
2023-12-01 23:07:54 +0700

    add simple redis

--- src/extensions.py
+++ src/extensions.py
@@ -3,4 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 mail = Mail()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri="redis://localhost:6379"
+)
